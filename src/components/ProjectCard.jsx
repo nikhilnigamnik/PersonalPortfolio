e356c4738862fd7569d1f6334fa65c4837f49b3f
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-05-17 17:36:41 +0530

    update card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -19,7 +19,8 @@ const cardsData = [
     id: 1,
     title: "Shopping Cart",
     description: "Where you can add and remove product from cart",
-    imageUrl: "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322711/Screenshot_3_ehhgz7.png",
+    imageUrl:
+      "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322711/Screenshot_3_ehhgz7.png",
     link: "https://shopping-cart-funs.vercel.app/",
   },
   {
@@ -27,7 +28,8 @@ const cardsData = [
     title: "Blog Application",
     description:
       "A fully functionaly blog application here you can see technology related blogs",
-    imageUrl: "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322710/Screenshot_2_n2aosg.png",
+    imageUrl:
+      "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322710/Screenshot_2_n2aosg.png",
     link: "https://codepoly.vercel.app/",
   },
   {
@@ -35,7 +37,8 @@ const cardsData = [
     title: "Gif Generator",
     description:
       "A gif generator reactJs application where you can find any type of gif by searching functionality",
-    imageUrl: "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322711/Screenshot_5_wpzjki.png",
+    imageUrl:
+      "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322711/Screenshot_5_wpzjki.png",
     link: "https://gifgenerator.vercel.app/",
   },
   {
@@ -43,13 +46,14 @@ const cardsData = [
     title: "Password Generator",
     description:
       "It helps users create stronger passwords that provide greater security for a given type of access.",
-    imageUrl: "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322710/Screenshot_4_bcj3q8.png",
+    imageUrl:
+      "https://res.cloudinary.com/dtmp7op6k/image/upload/v1684322710/Screenshot_4_bcj3q8.png",
     link: "https://passgenerator-two.vercel.app/",
   },
 ];
 
 // const CardPro = ({ id, title, description, link, imageUrl }) => (
-  
+
 //   <div key={id} className="card">
 //     <Card className="bg-white mt-10">
 //       <CardHeader shadow={false} floated={false} className="h-96">
@@ -84,16 +88,19 @@ const cardsData = [
 //   </div>
 // );
 
-const CardPro = () => {
+const CardPro = ({ imageUrl, title, description, link, id }) => {
   const [showText, setShowText] = useState(false);
 
   return (
-    <div className="relative max-w-sm mx-auto overflow-hidden rounded-lg shadow-lg">
+    <div
+      key={id}
+      className="relative max-w-sm mx-auto overflow-hidden rounded-lg shadow-lg"
+    >
       <img
-        className={`object-cover w-full h-64 transition duration-500 ease-in-out transform ${
-          showText ? "scale-110" : "scale-100"
+        className={`object-cover w-full h-64 transition duration-500 ease-in-out ${
+          showText ? "blur-md" : ""
         }`}
-        src="https://picsum.photos/seed/picsum/200/300"
+        src={imageUrl}
         alt="Card image"
       />
       <div
@@ -101,10 +108,11 @@ const CardPro = () => {
           showText ? "opacity-100" : "opacity-0"
         } transition-opacity duration-500 absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white`}
       >
-        <p className="text-2xl font-bold mb-2 animate-fadeIn">Card Text</p>
-        <a href="#" className="underline animate-fadeIn delay-200">
-          Learn More
-        </a>
+        <p className="text-2xl text-black font-bold mb-2 animate-fadeIn">{title}</p>
+          <Button className="bg-gray-900 shadow-none hover:shadow-none ">
+            See Live
+          </Button>
+        
       </div>
       <div
         className="absolute top-0 left-0 w-full h-full cursor-pointer"
@@ -113,7 +121,7 @@ const CardPro = () => {
       ></div>
     </div>
   );
-}
+};
 
 const ProjectCard = () => (
   <div className="grid sm:grid-cols-1  gap-6 xl:grid-cols-2 card">
